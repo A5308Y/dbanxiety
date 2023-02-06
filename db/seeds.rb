7ca22793bb4a2a3196222d749da9a86bb928c6e2
Andreas Hellwig <a5308y@gmail.com>
2023-02-06 20:45:15 +0100

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+1000.times do
+  order = Order.create!(description: 'Order', price: Random.rand(1000..100_000))
+  Random.rand(1..5).times do
+    LineItem.create!(description: 'LineItem', price: 1, order:)
+  end
+end
